c20b9dc408912576d1ad9a1e75cd0c55ad149d68
prescottbreeden <prescott.breeden@gmail.com>
2019-03-23 19:06:37 -0700

    edited server.js
    added some comments to ease understanding for students, renamed file to
    what students are used to seeing, added console.log joke for them

--- server.js
+++ server.js
@@ -3,37 +3,31 @@ const https = require('https');
 const path = require('path');
 const port = 1337;
 const app = express();
-const server = app.listen(port, ()=> console.log(`listening port ${port}`));
+const server = app.listen(port, ()=> console.log(`Listening on port ${port}`));
 const io = require('socket.io').listen(server);
-const rooms = {}; // temp fake database
+const rooms = {}; // temporary fake database
 
 app.use(express.static(path.join(__dirname, '/public/dist/public')));
 
 io.on("connection", socket => {
-
-  console.log('new connection');
-  socket.emit('herro', {msg: 'thanks for joining!'});
-
+  console.log('Rubber Baby Buggy Bumpers');
   let previousId;
+
+  // change channel of socket
   const safeJoin = currentId => {
     socket.leave(previousId);
     socket.join(currentId);
     previousId = currentId;
   };
 
-  socket.on('balls', data => {
-    console.log(data);
-  })
-
   socket.on("getRoom", roomId => {
-    safeJoin(roomId);
+    safeJoin(roomId); // change socket channel
     socket.emit("room", rooms[roomId]);
   });
 
   socket.on("addRoom", room => {
-    console.log(room);
-    rooms[room.id] = room;
-    safeJoin(room.id);
+    rooms[room.id] = room; // add room to fake database
+    safeJoin(room.id); // change socket channel
     io.emit("rooms", Object.keys(rooms));
     socket.emit("room", room);
   });
